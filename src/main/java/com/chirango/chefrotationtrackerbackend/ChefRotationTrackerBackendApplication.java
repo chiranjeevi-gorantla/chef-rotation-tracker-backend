6f25329f08e6145441d46644a9a871324427712d
chiranjeevi_gorantla <chiranjeevigorantla@gmail.com>
2024-03-18 17:02:20 -0400

    Initial commit

---
+++ src/main/java/com/chirango/chefrotationtrackerbackend/ChefRotationTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.chirango.chefrotationtrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChefRotationTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChefRotationTrackerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chirango/chefrotationtrackerbackend/ChefRotationTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chirango.chefrotationtrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChefRotationTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
